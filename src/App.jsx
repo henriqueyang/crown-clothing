7bcec6b9d0482b2cf3fd2e9e30c75eddd8f50c14
henriqueyang <henriqueyang17@gmail.com>
2019-11-01 13:51:20 -0300

    change files name, updating app w/ a shop page and add collections components

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 
-import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATSPAGE</h1>
-  </div>
-);
+import HomePage from './pages/homepage';
+import ShopPage from './pages/shop';
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/chapeus" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </>
   );
