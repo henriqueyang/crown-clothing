05f70889827e4339b8dee3a31c34c5911ef5770e
henriqueyang <henriqueyang17@gmail.com>
2019-11-05 16:33:59 -0200

    added icon and dropdown cart components

---
+++ src/store/actions/cart.js
@@ -0,0 +1,5 @@
+import CartActionTypes from '../types/cart';
+
+export const toggleCartHidden = () => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN,
+});
--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from '../types/user';
+import UserActionTypes from '../types/user';
 
 const INITIAL_STATE = {
   currentUser: null,
